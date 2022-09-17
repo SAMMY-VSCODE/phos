56e4b0031ce14d9a2f3a76c308fc0b1615aea219
SAMMY-VCODE <adekunlesammy10@gmail.com>
2022-09-17 01:30:20 -0700

    complete project

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var hamburger = document.querySelector('.hamburger-menu')
+var mobilelinks = document.querySelector('.mobile-link-holder')
+var backdrop = document.querySelector('.backdrop')
+
+function Addclass() {
+    hamburger.classList.toggle('show')
+    mobilelinks.classList.toggle('showlinks')
+    backdrop.classList.toggle('showlinks')
+}
+    
